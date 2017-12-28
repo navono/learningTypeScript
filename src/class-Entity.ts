30074a34f92980ddce6bc0b30fafa8b96b795b4e
navono <navono007@gmail.com>
2017-12-28 13:50:21 +0800

    :sparkles: add decorator

--- src/class-Entity.ts
+++ src/class-Entity.ts
@@ -1,3 +1,5 @@
+import { Log } from './log';
+
 export class Entity {
   private _id: number;
   private _title: string;
@@ -13,7 +15,9 @@ export class Entity {
     return this._id;
   }
 
+  @Log()
   get title(): string {
+    Entity.wait(1000);
     return this._title;
   }
 
@@ -23,5 +27,13 @@ export class Entity {
 
   get creationDate(): Date {
     return this._creationDate;
+  }
+
+  private static wait(ms: number) {
+    let start = Date.now();
+    let now = start;
+    while (now - start < ms) {
+      now = Date.now();
+    }
   }
 }
--- src/log.ts
+++ src/log.ts
@@ -1,4 +1,4 @@
-export function log() {
+export function Log() {
   return (target: Object, 
     propertyKey: string, 
     descriptor: TypedPropertyDescriptor<any>) => {
